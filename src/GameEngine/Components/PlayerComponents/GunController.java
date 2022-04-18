e7cd686e7172ad07c9ead1a31cae78a15d457c17
Robbie <robbiejohnwallace@yahoo.co.uk>
2022-04-18 14:35:37 +0100

    Added another placeholder class for the player.

---
+++ src/GameEngine/Components/PlayerComponents/GunController.java
@@ -0,0 +1,35 @@
+package GameEngine.Components.PlayerComponents;
+
+
+import GameEngine.Components.Component;
+import GameEngine.GameObjects.GameObject;
+
+
+public class GunController extends Component {
+   // Attributes
+   // Todo: implement, keeps track of all guns and which one is active
+   //       this component will also be responsible for firing the weapon
+   //       or maybe the character controller, not sure yet
+
+   // Constructor
+   public GunController(GameObject parent) {
+      super(parent);
+   }
+
+
+   // Methods 
+   public void start() {
+      // Todo: implement this function 
+      //       Called once when the parent game spawns object 
+   }
+
+   public void update() {
+      // Todo: implement this function 
+      //       Called every frame during update stage  
+   }
+
+   public void draw() {
+      // Todo: implement this function 
+      //       Called every frame during draw stage  
+   }
+}
